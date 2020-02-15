ce2e8ee26deb96d693955f76f5ecec905dccb687
Kyle <kyle.durigan@gmail.com>
2020-02-15 10:53:31 -0700

    Form for AddSale

--- salestracker/src/Components/FormControlCard.js
+++ salestracker/src/Components/FormControlCard.js
@@ -1,18 +1,45 @@
 import React from 'react';
-import { Button, Input, Table } from 'reactstrap';
+import { Button, Input, Table, Form, FormGroup, Label, FormText } from 'reactstrap';
 
 const FormControlCard = (props)=>{
-    console.log ('jamacian me crazy',  props)
     switch (props.type){
+
+        case 'addSaleForm':
+            return (
+                <Form>
+                    <FormGroup>
+                        <Label for='saleName'>Name</Label>
+                        <Input type='text' name='name' id='saleName' placeholder='Jane Doe' />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for='saleDate'>Date</Label>
+                        <Input type='date' name='date' id='saleDate' placeholder='6/1/2020' />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for='saleCID'>Customer ID#</Label>
+                        <Input type='number' name='cid' id='saleCID' placeholder='1234567' />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for='mebership'>Date</Label>
+                        <Input type="select" name="select" id="exampleSelect">
+                            {/* import options from props */}
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Input>
+                    </FormGroup>
+                </Form> 
+            )
         
         case 'textInput':
             return (
                 <Input
-                    onChange= {props.onChange}
+                    onChange= {props.handleChange}
                     placeholder= {props.placeholder ? props.placeholder : null}
                     value= {props.searchedValue}
                 />
-
             )
 
         default:
--- salestracker/src/Pages/AddSale.js
+++ salestracker/src/Pages/AddSale.js
@@ -5,67 +5,94 @@ import { Col, Row, Button, Input, Table } from 'reactstrap';
 import FormControlCard from '../Components/FormControlCard';
 
 export default class AddSale extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        
-      };
-  
-      // BIND THIS ACROSS FUNCTIONS
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: {
+        cid: '',
+        date: '',
+        membershipLevel: '',
+        name: '',
+        salesPerson: '',
+      },
+      searchedText: '',
+      userLevel: props.userLevel
+    };
+
+    // BIND THIS ACROSS FUNCTIONS
     //   this.getSummonerData = this.getSummonerData.bind(this);
     //   this.getMostRecentMatchData = this.getMostRecentMatchData.bind(this);
-    //   this.handleChange = this.handleChange.bind(this);
+      this.handleChange = this.handleChange.bind(this);
     //   this.handleSubmit = this.handleSubmit.bind(this);
-    let userLevel = props.userLevel;
+  }
+
+  handleChange(event) {
+    console.log(event.target.value)
+    let inputText = event.target.value;
+    this.setState({form: inputText});
+  }
+
+  //   handleSubmit(data) {
+
+  //     data.preventDefault();
+  //     this.setState({recentGameRequestFinished:false});
+  //     this.getSummonerData(this.state.player.searchName);
+  // }
+
+  renderForm(){
+
+  }
+
+  renderUserLevel(userLevel) {
+    switch (userLevel) {
+
+      case 'admin':
+        // ABILITY TO SWITCH BUISNESSES VIA SEARCH
+        // ABILITY TO SEE ALL USER DATA
+        // ABILITY TO EDIT ALL DATA (IF ENABLED)
+        return (
+          <>
+            <p>PROPS: {this.state.userLevel}</p>
+            <FormControlCard
+              handleChange={(data)=>this.handleChange(data)}
+              // placeholder= {props}
+              // value= {props}
+              type='addSaleForm'
+            />
+          </>
+        )
+
+      case 'manager':
+        // ABILITY TO SEE ALL USER DATA
+        // ABILITY TO ADD/EDIT ALL DATA
+        return (
+          // <input
+          <p>PROPS: {this.state.userLevel}</p>
+          // />
+        )
+
+      case 'user':
+        // ABILITY TO ADD/EDIT DATA ASSOCIATED WITH USER
+        // ABIILITY TO VIEW DATA ASSOCIATED WITH USER
+        return (
+          // <input
+          <p>PROPS: {this.state.userLevel}</p>
+          // />
+        )
+
+      default:
+        return (console.log(`[ERROR]: NO USER LEVEL DEFINED`))
+
     }
+  }
 
-    handleChange(data) {
+  render() {
+    return(
+      <>
+        {this.renderUserLevel(this.state.userLevel)}
+      </>
+    )
 
-    //     let player = { ...this.state.player };
-    //     player.searchName = data.target.value;
-    //     this.setState({ player });
-    }
-    
-    //   handleSubmit(data) {
-    
-    //     data.preventDefault();
-    //     this.setState({recentGameRequestFinished:false});
-    //     this.getSummonerData(this.state.player.searchName);
-    // }
-
-    render (userLevel) {
-        switch (userLevel){
-
-            case 'admin': 
-                return (
-                    // <p>PROPS: {this.state.userLevel}</p>
-                    <FormControlCard
-                        onChange= {data=>this.handleChange(data)}
-                        // placeholder= {props}
-                        // value= {props}
-                        type='textInput'
-                    />
-                )
-
-            case 'manager':
-                return (
-                    // <input
-                    <p>PROPS: {this.state.userLevel}</p>
-                    // />
-                )
-
-            case 'user':
-                return (
-                    // <input
-                    <p>PROPS: {this.state.userLevel}</p>
-                    // />
-                )
-
-            default:
-                return (console.log(`[ERROR]: NO USER LEVEL DEFINED`))
-
-        }
-
-        }
+  }
 
 }
