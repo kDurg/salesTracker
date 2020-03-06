43de938f00c763470da75170de837995187db8a7
Kyle Durigan <kyle.durigan@gmail.com>
2020-03-06 15:39:29 -0700

    Basic API test set up

--- salestracker/src/App.js
+++ salestracker/src/App.js
@@ -8,20 +8,41 @@ import AddSale from './Pages/AddSale';
 //IMPORT COMPONENTS
 // import FormControlCard from './Components/FormControlCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Sales Tracker
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiResponse: '',
+
+    }
+  }
+
+  callAPI() {
+    fetch('http://localhost:9000/testAPI')
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }))
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Sales Tracker
         </p>
-      </header>
+        </header>
+        <h1>{this.state.apiResponse}</h1>
         <AddSale
-          userFriendlyName= 'Becky D.'
+          userFriendlyName='Becky D.'
           userLevel='admin'
         />
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
