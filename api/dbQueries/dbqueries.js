13ccc0f38a00c5c9a64d943c0015f7708193cef1
Kyle Durigan <kyle.durigan@gmail.com>
2020-03-19 17:08:10 -0600

    fix to sql syntax when adding params

--- api/dbQueries/dbqueries.js
+++ api/dbQueries/dbqueries.js
@@ -18,7 +18,7 @@ const database = (type, data) => {
             let userData = data.user;
             let saleData = data.data
             let insertStatement = `INSERT INTO ${dbName}.salesdata (companyid, locationid, userid, param1, param2, param3, param4, param5) `; //, param6, param7, param8, param9, param10
-            let values = `VALUES (${userData.companyID}, ${userData.locationID}, ${userData.userID}, ${saleData.cid}, ${saleData.date}, '${saleData.name}', '${saleData.membershipLevel}', '${saleData.salesperson}');`
+            let values = `VALUES (${userData.companyID}, ${userData.locationID}, ${userData.userID}, '${saleData.cid}', '${saleData.date}', '${saleData.name}', '${saleData.membershipLevel}', '${saleData.salesperson}');`
             let query = insertStatement + values;
             
             dbConnection.query(query, (err, res)=> {
